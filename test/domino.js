7e0e4ce99be2cebc03930dd831301e162dcdd4cd
Alan Agius <alanagius@google.com>
2023-03-17 15:00:14 +0000

    fix: do not emit known invalid values
    This commit adds a fix so that known invalid values are not added. This is so that Domino mimics more the browser behaviour were known invalid values are not added.

--- test/domino.js
+++ test/domino.js
@@ -1461,3 +1461,10 @@ exports.incrementalHTMLParser2 = function() {
     '<html><head></head><body><p>hello<b>foo&amp;</b></p></body></html>'
   );
 };
+
+exports.quotedUnitValues = function() {
+  var document = domino.createDocument('<div></div>');
+  var div = document.querySelector('div');
+  div.style.width = '"0px"';
+  div.outerHTML.should.equal('<div></div>');
+};
