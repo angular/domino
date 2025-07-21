e2c9292207511528cad0772647a36b48459f2228
Joey Perrott <josephperrott@gmail.com>
2025-07-21 20:42:28 +0000

    test

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,4 +2,6 @@ declare module 'domino' {
   function createDOMImplementation(): DOMImplementation;
   function createDocument(html?: string, force?: boolean): Document;
   function createWindow(html?: string, address?: string): Window;
+  const impl: any;
+  const Event: any;
 }
